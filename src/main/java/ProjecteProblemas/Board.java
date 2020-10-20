11b9ebdca79ac454d6eff431a7d26bca05d55a8f
Aleix99 <aleix.torresr@e-campus.uab.cat>
2020-10-20 13:47:15 +0200

    Definicion de clases y clases test empezando crear metodos y variables

--- src/main/java/ProjecteProblemas/Board.java
+++ src/main/java/ProjecteProblemas/Board.java
@@ -1,10 +1,55 @@
 package ProjecteProblemas;
 
 public class Board extends MockBoard {
-
-	 private int squares[];
+	
+	private int size;
+	private int nMines;
+	private int difficulty;
+	private long score;
+	private int squares[][];
 	
 	 Board() {
+		 size = 0;
+		 difficulty= 1;
+		 score=0;
+		 for(int i = 0; i < size; i++) {
+			 for(int j = 0; j < size; j++) {
+				 squares[i][j]=0;
+			 }
+		 }
+	 }
+	 
+	 public void setBoard(int difficult) {
+		 difficulty = difficult;
+		switch (difficult) {
+		case(1): //easy
+			size=10;
+			nMines=10;
+			break;
+		case(2): //normal
+			size=16;
+			nMines=40;
+			break;
+		case(3): //hard
+			size=20;
+			nMines=100;
+			break;
+		default:
+		}
+		
+		for(int i = 0; i < size; i++) {
+			 for(int j = 0; j < size; j++) {
+				 squares[i][j]=0; //0=normal number
+			 }
+		 }
+		
+	 }
+	 
+	 public void restartBoard() {
+		 
+	 }
+	 
+	 public void generateRandomMines() {
 		 
 	 }
 }
---
+++ src/test/java/ProjecteProblemas/BoardTest.java
@@ -0,0 +1,32 @@
+package ProjecteProblemas;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BoardTest {
+
+	@Test
+	public void testBoard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetBoard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRestartBoard() {
+		Board b = new Board();
+		b.size;
+		
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGenerateRandomMines() {
+		fail("Not yet implemented");
+	}
+
+}
