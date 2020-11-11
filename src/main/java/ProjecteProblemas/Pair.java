8f6788ea06f9447167be87c8b2780c4e30bb8b8d
Fernandop26 <fernandop135@gmail.com>
2020-11-11 20:02:07 +0100

    .

---
+++ src/main/java/ProjecteProblemas/Pair.java
@@ -0,0 +1,13 @@
+package ProjecteProblemas;
+
+public class Pair { 
+    int x; 
+    int y; 
+  
+    // Constructor 
+	public Pair(int x, int y) 
+	    { 
+	        this.x = x; 
+	        this.y = y; 
+	    } 
+} 
